ffd36eae9c2d384a9ea7fcdccfdc779bd77be698
thdus1323 <thdus1323@naver.com>
2025-01-25 02:33:15 +0900

    feat/컨트롤러 완료/상품목록 구현

---
+++ src/main/java/jpabook/fineday_payment/product/ProductController.java
@@ -0,0 +1,15 @@
+package jpabook.fineday_payment.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class ProductController {
+
+    @GetMapping("/")
+    public String productList() {
+        return "productList";
+    }
+    }
